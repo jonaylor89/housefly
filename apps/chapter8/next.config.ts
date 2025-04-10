2a0a8d38542e486a52bd7344a4b209e50f5e03a5
Johannes Naylor <jonaylor89@gmail.com>
2025-04-09 21:06:18 -0400

    fix next build

--- apps/chapter8/next.config.ts
+++ apps/chapter8/next.config.ts
@@ -1,4 +1,8 @@
 import type { NextConfig } from "next";
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+
+// import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
   webpack: (config, { isServer }) => {
