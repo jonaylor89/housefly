19b35bbc553f5ecc9c8d5bfcc41277a7bedf89ac
Johannes Naylor <jonaylor89@gmail.com>
2025-04-16 21:38:36 -0400

    fix tutorial

--- apps/chapter9/next.config.js
+++ apps/chapter9/next.config.js
@@ -3,6 +3,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    // Add problematic directories to exclusions
+    config.watchOptions = {
+      ignored: ["**/node_modules", "**/.git", "**/.next"],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
